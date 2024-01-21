f5e94d6b90ec731354ead456edb3d2fd232330d0
Minchelin42 <mje0211@naver.com>
2024-01-22 00:32:00 +0900

    DESIGN: tabBar tintColor 적용

---
+++ SeSACShopping/MainTabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  MainTabBarController.swift
+//  SeSACShopping
+//
+//  Created by 민지은 on 2024/01/22.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.tabBar.tintColor = .pointColor
+        self.tabBar.unselectedItemTintColor = .darkGray
+        
+    }
+
+}
